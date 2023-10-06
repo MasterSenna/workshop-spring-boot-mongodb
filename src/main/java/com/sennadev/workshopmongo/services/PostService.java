daba1b965aa8713cb886418d8761d87e109bfa74
Felipe Sena <felipesenna180@gmail.com>
2023-10-06 10:23:09 -0300

    projeto finalizado

---
+++ src/main/java/com/sennadev/workshopmongo/services/PostService.java
@@ -0,0 +1,34 @@
+package com.sennadev.workshopmongo.services;
+
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sennadev.workshopmongo.domain.Post;
+import com.sennadev.workshopmongo.repository.PostRepository;
+import com.sennadev.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public List<Post> findByTitle(String text) {
+		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
+} 
